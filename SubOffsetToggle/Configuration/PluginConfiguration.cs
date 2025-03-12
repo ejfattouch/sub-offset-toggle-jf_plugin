ab534c6b166ef7e16bbd0f085b581ca419fc69ac
ejfattouch <ejfattouch@outlook.com>
2025-03-12 00:44:04 +0000

    First commit

---
+++ SubOffsetToggle/Configuration/PluginConfiguration.cs
@@ -0,0 +1,29 @@
+using MediaBrowser.Model.Plugins;
+
+namespace SubOffsetToggle.Configuration;
+
+/// <summary>
+/// Plugin configuration.
+/// </summary>
+public class PluginConfiguration : BasePluginConfiguration
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="PluginConfiguration"/> class.
+    /// </summary>
+    public PluginConfiguration()
+    {
+        // set default options here
+        Offset1 = 0;
+        Offset2 = 0;
+    }
+
+    /// <summary>
+    /// Gets or sets the first offset.
+    /// </summary>
+    public float Offset1 { get; set; }
+
+    /// <summary>
+    /// Gets or sets the second offset.
+    /// </summary>
+    public float Offset2 { get; set; }
+}
