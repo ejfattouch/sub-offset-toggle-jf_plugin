6055a40e68084a5b5cbfc7529cf063365ba35003
ejfattouch <ejfattouch@outlook.com>
2025-03-18 04:27:06 +0000

    Finished functionality to upload custom js script to web server. TODO: Find a way for plugin to gain access to file directory

--- SubOffsetToggle/Plugin.cs
+++ SubOffsetToggle/Plugin.cs
@@ -1,11 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
+using Microsoft.Extensions.Logging;
 using SubOffsetToggle.Configuration;
+using SubOffsetToggle.Templating;
 
 namespace SubOffsetToggle;
 
@@ -14,15 +17,23 @@ namespace SubOffsetToggle;
 /// </summary>
 public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
 {
+    private readonly ILogger<Plugin> _logger;
+
     /// <summary>
     /// Initializes a new instance of the <see cref="Plugin"/> class.
     /// </summary>
     /// <param name="applicationPaths">Instance of the <see cref="IApplicationPaths"/> interface.</param>
     /// <param name="xmlSerializer">Instance of the <see cref="IXmlSerializer"/> interface.</param>
-    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+    /// <param name="logger">Instance of the <see cref="ILogger{Plugin}"/> interface, used for logging messages related to this plugin.</param>
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer, ILogger<Plugin> logger)
         : base(applicationPaths, xmlSerializer)
     {
+        _logger = logger;
+        _logger.LogInformation("Sub Offset Toggle Plugin has been initialized.");
+
         Instance = this;
+
+        ConfigurationChanged += OnConfigurationChanged;
     }
 
     /// <inheritdoc />
@@ -36,6 +47,20 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
     /// </summary>
     public static Plugin? Instance { get; private set; }
 
+    /// <summary>
+    /// Handles the event when the plugin configuration is changed.
+    /// This method is triggered whenever the user updates and saves the plugin settings.
+    /// </summary>
+    private void OnConfigurationChanged(object? sender, BasePluginConfiguration config)
+    {
+        _logger.LogInformation("OnConfigurationChanged has been called");
+
+        if (config is PluginConfiguration pluginConfiguration)
+        {
+            RenderJSInjectionTemplate(pluginConfiguration);
+        }
+    }
+
     /// <inheritdoc />
     public IEnumerable<PluginPageInfo> GetPages()
     {
@@ -47,5 +72,16 @@ public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
                 EmbeddedResourcePath = string.Format(CultureInfo.InvariantCulture, "{0}.Configuration.configPage.html", GetType().Namespace)
             }
         ];
+    }
+
+    private void RenderJSInjectionTemplate(PluginConfiguration config)
+    {
+        _logger.LogInformation("RenderJSInjectionTemplate method has been called");
+        string web_dir = "/usr/share/jellyfin/web/";
+        string inject_dir = web_dir + "injection/";
+
+        FileInfo output = new FileInfo(inject_dir + "offset_btn_injection.js");
+        JSInjectionTemplater templater = new JSInjectionTemplater(output, _logger);
+        templater.ProcessInjectedJS(config.Offset1, config.Offset2);
     }
 }
--- SubOffsetToggle/Templating/JSInjectionTemplater.cs
+++ SubOffsetToggle/Templating/JSInjectionTemplater.cs
@@ -1,19 +1,100 @@
 using System.IO;
+using System.Reflection;
+using Microsoft.Extensions.Logging;
 
 namespace SubOffsetToggle.Templating
 {
     /// <summary>
-    /// Handles file templating.
+    /// Handles templating the JavaScript code that will be injected in the Media Playback.
     /// </summary>
-    public class JSInjectionTemplater : Templater
+    public class JSInjectionTemplater
     {
+        private readonly ILogger<Plugin> _logger;
+        private readonly Templater _templater;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="JSInjectionTemplater"/> class.
         /// Requires an inject_template.js file present in the templates subdirectory of this class.
         /// </summary>
         /// <param name="output">Output file.</param>
-        public JSInjectionTemplater(FileInfo output) : base(new FileInfo("templates/inject_template.js"), output)
+        /// <param name="logger">Logger passed on from Plugin.</param>
+        public JSInjectionTemplater(FileInfo output, ILogger<Plugin> logger)
         {
+            _logger = logger;
+            _logger.LogInformation("JSInjectionTemplater Constructor");
+
+            string templateContent = ExtractTemplate();
+            SaveTemplateToFile(templateContent);
+
+            FileInfo templatePath = new FileInfo("/tmp/templates/inject_template.js");
+            if (!templatePath.Exists)
+            {
+                throw new FileNotFoundException("/tmp/templates/inject_template.js was not supplied");
+            }
+
+            _templater = new Templater(templatePath, output);
+        }
+
+        /// <summary>
+        /// Extracts the template file from the Embedded resources.
+        /// </summary>
+        /// <returns>String representation of the template file.</returns>
+        public static string ExtractTemplate()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+            // Define the embedded resource name (namespace + folder structure + file name)
+            string resourceName = "SubOffsetToggle.Templating.templates.inject_template.js";
+
+            // Extract the resource to a string
+            using (Stream? resourceStream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (resourceStream == null)
+                {
+                    throw new FileNotFoundException("Embedded resource not found", resourceName);
+                }
+
+                // Read the stream into a string (assuming it's text)
+                using (StreamReader reader = new StreamReader(resourceStream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Processes the template and saves the rendered output.
+        /// </summary>
+        /// <param name="templateContent">String value of the contents of the template file.</param>
+        public void SaveTemplateToFile(string templateContent)
+        {
+            try
+            {
+                // Define the output file path
+                string outputPath = "/tmp/templates/inject_template.js";
+
+                // Ensure the directory exists
+                string? directory = Path.GetDirectoryName(outputPath);
+
+                if (directory is null)
+                {
+                    throw new DirectoryNotFoundException("Directory could not be found from the string");
+                }
+
+                if (!Directory.Exists(directory))
+                {
+                    Directory.CreateDirectory(directory);  // Create the directory if it doesn't exist
+                }
+
+                // Write the template content to the file
+                File.WriteAllText(outputPath, templateContent);
+
+                _logger.LogInformation("Template saved to {OutputPath}", outputPath);
+            }
+            catch (System.Exception ex)
+            {
+                _logger.LogError("Error saving template: {Error}", ex.Message);
+            }
         }
 
         /// <summary>
@@ -29,7 +110,7 @@ namespace SubOffsetToggle.Templating
                 offset_value_2 = offset2
             };
 
-            ProcessTemplate(data);
+            _templater.ProcessTemplate(data);
         }
     }
 }
